4db2042bbcd100bcea72699d296da96ba181e039
Andy <andymendez100@gmail.com>
2019-10-11 15:16:31 -0700

    Seperated calculator functions to another namespace, Trying to fix weird bug that affects error handeling

---
+++ firstcSharp/firstcSharp/Calculations/Calculations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace firstcSharp.Calculator
+{
+    public class Calculations
+    {
+        public static void Add(int num1, int num2)
+        {
+            int result = num1 + num2;
+            Console.WriteLine($"The answer is {result}");
+
+
+        }
+        public static void Subtract(int num1, int num2)
+        {
+            int result = num1 - num2;
+
+            Console.WriteLine($"The answer is {result}");
+        }
+        public static void Multiply(int num1, int num2)
+        {
+            int result = num1 * num2;
+
+            Console.WriteLine($"The answer is {result}");
+        }
+        public static void Divide(int num1, int num2)
+        {
+            double result = num1 / num2;
+
+            Console.WriteLine($"The answer is {result}");
+        }
+    }
+}
--- firstcSharp/firstcSharp/Program.cs
+++ firstcSharp/firstcSharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using firstcSharp.Calculator;
 
 namespace firstcSharp
 {
@@ -6,42 +7,98 @@ namespace firstcSharp
     {
         static void Main(string[] args)
         {
-
+            Start();
+        }
+        static void Start()
+        {
             string userInput;
 
-            Console.WriteLine("What type of calculation do you want to perform? (+, -, *, or /)");
-            string userChoice = Console.ReadLine();
 
-            Console.WriteLine("Enter first number");
-            userInput = Console.ReadLine();
-            int numberOne = Convert.ToInt32(userInput);
+            string userChoice = Calculation();
 
-            Console.WriteLine("Enter second number");
-            userInput = Console.ReadLine();
-            int numberTwo = Convert.ToInt32(userInput);
+            int numberOne = FirstNum();
+
+
+            int numberTwo = SecondNum();
 
 
             switch (userChoice)
             {
                 case "+":
-                    Console.WriteLine($"The answer is {numberOne + numberTwo}");
+                    Calculations.Add(numberOne, numberTwo);
+                    Restart();
+
                     break;
                 case "-":
-                    Console.WriteLine($"The answer is {numberOne - numberTwo}");
+                    Calculations.Subtract(numberOne, numberTwo);
+                    Restart();
                     break;
                 case "*":
-                    Console.WriteLine($"The answer is {numberOne * numberTwo}");
+                    Calculations.Multiply(numberOne, numberTwo);
+                    Restart();
                     break;
                 case "/":
                     double total = numberOne / numberTwo;
-                    Console.WriteLine($"The answer is {total}");
+                    Calculations.Divide(numberOne, numberTwo);
+                    Restart();
                     break;
                 default:
-                    Console.WriteLine("Please insert the right type");
+                    Console.WriteLine("Hmm this is an interesting Bug...");
                     break;
             }
+        }
+        static string Calculation()
+        {
+            Console.WriteLine("What type of calculation do you want to perform? (+, -, *, or /)");
+
+            string answer = Console.ReadLine();
+            if (answer != "+" && answer != "-" && answer != "*" && answer != "/")
+            {
+                Console.WriteLine("Please pick something that is actually an option...");
+                Calculation();
+            }
+
+            return answer;
 
 
+        }
+        static int FirstNum()
+        {
+            int value;
+            Console.WriteLine("Enter first number");
+            string userInput = Console.ReadLine();
+            if (!int.TryParse(userInput, out value))
+            {
+                Console.WriteLine("Please insert a correct number -.-");
+                FirstNum();
+            }
+            return value;
+        }
+        static int SecondNum()
+        {
+            int value;
+            Console.WriteLine("Enter second number");
+            string userInput = Console.ReadLine();
+            if (!int.TryParse(userInput, out value))
+            {
+                Console.WriteLine("Please insert a correct number -.-");
+                SecondNum();
+            }
+            return value;
+        }
+        static void Restart()
+        {
+            Console.WriteLine("Would you like to restart? Press y to restart");
+            string userAnswer = Console.ReadLine();
+
+            if (userAnswer == "y")
+            {
+                Start();
+            }
+            else
+            {
+                Console.WriteLine("Wack");
+            }
         }
     }
 }
