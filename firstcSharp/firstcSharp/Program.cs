e34866b156512a1689c5e671b50418827b116809
Andy <andymendez100@gmail.com>
2019-10-10 19:18:35 -0700

    Adding files

---
+++ firstcSharp/firstcSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace firstcSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+
+            Add(1 + 2);
+        }
+
+        static void Add(int firstNum, int secNum )
+        {
+            int total = firstNum + secNum;
+            Console.WriteLine(total);
+            
+        }
+    }
+}
